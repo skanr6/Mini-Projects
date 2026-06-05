640848228f7615950f0797e5301730715913c154
Skanda_R <bsrshetty06@gmail.com>
2026-06-05 23:32:41 +0530

    Updated Student-Management-System-miniproject

--- Student-Management-System.cpp
+++ Student-Management-System.cpp
@@ -11,18 +11,34 @@ struct student
 };
 vector<student>vec;
 
+//load data
+void load_data()
+{
+    ifstream fin("students.txt");
+    student s;
+    while(fin>>s.id>>s.name>>s.marks)
+    {
+        vec.push_back(s);
+    }
+    fin.close();
+}
+
 //addition
 void addst()
 {
-student s;
-cout<<" Enter the student ID : ";
-cin>>s.id;
-cout<<" Enter the name of the student : ";
-cin>>s.name;
-cout<<" Enter the marks of the student : ";
-cin>>s.marks;
-vec.push_back({s.id,{s.name,s.marks}});
-cout<<" Student added successfully !";
+    student s;
+    cout<<" \nEnter the student ID : ";
+    cin>>s.id;
+    cout<<"Enter the name of the student : ";
+    cin>>s.name;    
+    cout<<"Enter the marks of the student : ";
+    cin>>s.marks;
+    vec.push_back({s.id,s.name,s.marks});
+    ofstream fout("students.txt",ios::app);
+    fout<<s.id<<" "<<s.name<<" "<<s.marks<<endl;
+    fout.close();
+    cout<<" Student added successfully !";
+    cout<<"\n"<<"\n";
 }
 
 //display
@@ -85,10 +101,11 @@ void delst()
 int main()
 {
     int n;
-    cout<<"===== STUDENT MANAGEMENT SYSTEM ====="<<endl<<endl;
+    load_data();
+    cout<<endl;
+    cout<<"===== STUDENT MANAGEMENT SYSTEM ====="<<endl<<endl;;
     while(true)
     {
-    cout<<endl;
     cout<<" 1. Add Student \n 2. Display Students \n 3. Search Student \n 4. Delete Student \n 5. Exit "<<endl<<endl;
     cout<<"Enter your choice by typing the number : ";
     cin>>n;
